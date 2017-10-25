0680aa6516f870d1d95a2d0d610726b7352b1263
¨Kevin <kmarshall@telensa.com>
2017-10-25 09:05:24 +0100

    Added more logging config to include stream and file handlers

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 
-
 import logging
 import os
+
+import module
 
 
 def initLogging(logLevel=logging.DEBUG,logPath="logs/"):
@@ -11,14 +12,23 @@ def initLogging(logLevel=logging.DEBUG,logPath="logs/"):
     """
 
     logPath = os.path.join(logPath,"MainLog.log")
-    logging.basicConfig(level=logLevel,
-                        handlers=[logging.FileHandler(logPath),
-                                  logging.StreamHandler()])
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler(logPath)
+    fh.setLevel(logLevel)
+    fh.setFormatter(formatter)
+    ch = logging.StreamHandler()
+    ch.setLevel(logLevel)
+    ch.setFormatter(formatter)
+    l = logging.getLogger()
+    l.addHandler(ch)
+    l.addHandler(fh)
+    l.setLevel(logLevel)
 
 if __name__ == "__main__":
 
     initLogging()
-    log=logging.getLogger()
+    log=logging.getLogger(__name__)
 
-    log.info('Starting %s'%__name__)
+
     
