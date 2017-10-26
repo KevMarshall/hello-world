70cc171f5da7be3eb1beaffd8b73fda44fbd580b
¨Kevin <kmarshall@telensa.com>
2017-10-26 09:22:17 +0100

    Added ability to load json logging config file

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 
 
 import logging
+import logging.config
 import os
+import json
 
-import module
 
-
-def initLogging(logLevel=logging.DEBUG,logPath="logs/"):
+def initLogging(useConfFile = False, logLevel=logging.DEBUG,logPath="logs/"):
     """
     Sets up the basic logging
     """
 
-    logPath = os.path.join(logPath,"MainLog.log")
+    if useConfFile:
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh = logging.FileHandler(logPath)
-    fh.setLevel(logLevel)
-    fh.setFormatter(formatter)
-    ch = logging.StreamHandler()
-    ch.setLevel(logLevel)
-    ch.setFormatter(formatter)
-    l = logging.getLogger()
-    l.addHandler(ch)
-    l.addHandler(fh)
-    l.setLevel(logLevel)
+        logConfPath = 'cfg/loggingConfig.json'
+
+        if os.path.exists(logConfPath):
+            
+            with open(logConfPath,'rt') as cnfFile:
+                config = json.load(cnfFile)
+
+            logging.config.dictConfig(config)
+                
+
+    else:
+        
+        logPath = os.path.join(logPath,"MainLog.log")
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh = logging.FileHandler(logPath)
+        fh.setLevel(logLevel)
+        fh.setFormatter(formatter)
+        ch = logging.StreamHandler()
+        ch.setLevel(logLevel)
+        ch.setFormatter(formatter)
+        l = logging.getLogger()
+        l.addHandler(ch)
+        l.addHandler(fh)
+        l.setLevel(logLevel)
 
 if __name__ == "__main__":
 
-    initLogging()
+    
+
+    initLogging(useConfFile=True)
     log=logging.getLogger(__name__)
+    print log
+    log.info("Main Starting...")
 
 
     
